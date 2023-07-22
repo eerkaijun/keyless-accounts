e4f45be0a27d7e314f824eb0b5760c29c613bf70
eerkaijun <kaijuneer@gmail.com>
2023-07-23 00:34:49 +0200

    test successful

--- test/Account.t.sol
+++ test/Account.t.sol
@@ -5,20 +5,32 @@ import "forge-std/Test.sol";
 import "forge-std/console.sol";
 import "../src/Account.sol";
 import "../src/AccountFactory.sol";
+import "../src/MockContract.sol";
 
 contract AccountTest is Test {
 
     AccountFactory public factory;
     SmartAccount public account;
+    MockContract public mockContract;
 
     function setUp() public {
         factory = new AccountFactory(IEntryPoint(address(0x0)));
         // in real life, we would pass the address of the lit protocol pkp as the owner
         account = factory.createAccount(address(this), 0);
+        mockContract = new MockContract();
     }
 
     function testAccount() external {
         // send a userOp to mint an nft from mock contract
+        bytes memory functionSignature = abi.encodeWithSignature(
+            "mint(address,uint256)", 
+            address(account),
+            1 // tokenId of 1
+        );
 
+        account.execute(address(mockContract), 0, functionSignature);
+
+        // check that the nft is owned by the account
+        assertEq(mockContract.ownerOf(1), address(account), "nft should be owned by account");
     }
 }
